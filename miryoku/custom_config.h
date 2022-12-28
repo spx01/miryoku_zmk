f07041921b778c7675dbf0a83fe1c94a9050fdf7
spx01 <>
2022-12-28 20:12:57 +0200

    add half transparent layers

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,34 +3,6 @@
 
 #define XXX &none
 
-#if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
-#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
-     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
-)\
-XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
-&kp SEMI  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  &kp BSLH  \
-XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
-               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
-#else
-#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
-     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
-)\
-XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
-&kp SEMI  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  &kp BSLH  \
-XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
-               XXX  K32  K33  K34  XXX       XXX  K35  K36  K37  XXX
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 42 43
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 46 47
-#endif
-
 #define MIRYOKU_POSITIONAL_MODS
 #define MIRYOKU_POSITIONS_LEFT_KYRIA 0 1 2 3 4 5 12 13 14 15 16 17 24 25 26 27 28 29 30 31 40 41 42 43 44 45 46 47 48 49
 #define MIRYOKU_POSITIONS_RIGHT_KYRIA 6 7 8 9 10 11 18 19 20 21 22 23 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49
@@ -46,9 +18,10 @@ MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(SYM2,   "Sym2")
+MIRYOKU_X(SYM2,   "Sym2") \
+MIRYOKU_X(LHALF,  "LHalf") \
+MIRYOKU_X(RHALF,  "RHalf")
 
-#define MIRYOKU_LAYERMAPPING_SYM2 MIRYOKU_MAPPING
 #define U_BASE   0
 #define U_EXTRA  1
 #define U_TAP    2
@@ -60,6 +33,12 @@ MIRYOKU_X(SYM2,   "Sym2")
 #define U_SYM    8
 #define U_FUN    9
 #define U_SYM2   10
+#define U_LHALF  11
+#define U_RHALF  12
+
+#define MIRYOKU_LAYERMAPPING_SYM2 MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_LHALF MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_RHALF MIRYOKU_MAPPING
 
 #define MIRYOKU_LAYER_SYM2 \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_NA,              &kp MINUS,         &kp EQUAL,         &kp PLUS,          U_NA,           \
@@ -72,3 +51,44 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 U_MTL(LGUI, A),    U_MTL(LALT, R),    U_MTL(LCTRL, S),    U_MTL(LSHFT, T),   &kp G,              &kp M,             U_MTR(LSHFT, N),   U_MTR(LCTRL, E),   U_MTR(LALT, I),    U_MTR(LGUI, O),       \
 U_LT(U_BUTTON, Z), U_MTL(RALT, X),    &kp C,              &kp D,             &kp V,              &kp K,             &kp H,             &kp COMMA,         U_MTR(RALT, DOT),  U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPC),  U_LT(U_SYM2, TAB),  U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_LHALF \
+XXX, XXX, XXX, XXX, XXX,     U_NA, &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+XXX, XXX, XXX, XXX, XXX,     U_NA, &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+XXX, XXX, XXX, XXX, XXX,     U_NA,  U_NA,             U_NA,              &kp RALT,          U_NA,              \
+XXX, XXX, XXX, XXX, XXX,     U_NA, U_NA,              U_NA,              U_NP,              U_NP
+
+
+#define MIRYOKU_LAYER_RHALF \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,      XXX, XXX, XXX, XXX, XXX \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,      XXX, XXX, XXX, XXX, XXX \
+U_NA,              &kp RALT,          U_NA,              U_NA,              U_NA,      XXX, XXX, XXX, XXX, XXX \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,      XXX, XXX, XXX, XXX, XXX
+
+#if defined MIRYOKU_MAPPING_EXTENDED_THUMBS
+#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
+     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+)\
+XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
+&kp SEMI  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  &kp BSLH  \
+XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
+               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
+#else
+#define MIRYOKU_LAYOUTMAPPING_KYRIA(\
+     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+)\
+XXX  K00  K01  K02  K03  K04                                          K05  K06  K07  K08  K09  XXX  \
+&kp SEMI  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19  &kp BSLH  \
+XXX  K20  K21  K22  K23  K24  XXX  XXX                           XXX  XXX  K25  K26  K27  K28  K29  XXX  \
+               XXX  K32  K33  K34  &mo U_RHALF      &mo U_LHALF  K35  K36  K37  XXX
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 42 43
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 46 47
+#endif
